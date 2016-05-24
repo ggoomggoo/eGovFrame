fe0d919684e2a9e566c31ff047ca294f496fc0e1
t1t1 <cha@hanmail2.net>
2016-05-24 17:11:26 +0900

    create project; Persistence Layer; ibatis

---
+++ lab204-ibatis/src/main/java/egovframework/lab/dataaccess/service/EmpService.java
@@ -0,0 +1,20 @@
+package egovframework.lab.dataaccess.service;
+
+import java.math.BigDecimal;
+import java.util.List;
+
+public interface EmpService {
+    
+    // [Step 2-1] EmpService 작성
+        
+    public BigDecimal insertEmp(EmpVO empVO) throws Exception;
+
+    public void updateEmp(EmpVO empVO) throws Exception;
+
+    public void deleteEmp(EmpVO empVO) throws Exception;
+
+    public EmpVO selectEmp(EmpVO empVO) throws Exception;
+    
+    public List<EmpVO> selectEmpList(EmpVO searchVO) throws Exception;
+    
+}
